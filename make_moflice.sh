29938633dab0b7c4c2d3eb63e5a7e20b06a1ad49
jack2game <rex.txt@gmail.com>
2024-04-17 13:20:27 +0200

    initial

---
+++ make_moflice.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# BUILD_TYPE="$1"
+
+# 生成繁體
+
+rm -rf moflice-cht
+cp -a ./rime-moran/. ./moflice-cht
+
+rm ./moflice-cht/default.yaml
+rm ./moflice-cht/key_bindings.yaml
+rm ./moflice-cht/punctuation.yaml
+
+cp ./rime-moran/tools/data/zrmdb.txt ./tools-additional
+sed -i 's/ /\t/g' ./tools-additional/zrmdb.txt
+
+mkdir -p ./moflice-cht/ice-dicts/
+
+cd ./tools-additional
+python3 gen_dict_with_shape.py -i ../rime-ice/cn_dicts/8105.dict.yaml    -x zrmdb -t -o ../moflice-cht/ice-dicts/flypy_zrmdb_8105.dict.yaml
+python3 gen_dict_with_shape.py -i ../rime-ice/cn_dicts/41448.dict.yaml   -x zrmdb -t -o ../moflice-cht/ice-dicts/flypy_zrmdb_41448.dict.yaml
+python3 gen_dict_with_shape.py -i ../rime-ice/cn_dicts/base.dict.yaml    -x zrmdb -t -o ../moflice-cht/ice-dicts/flypy_zrmdb_base.dict.yaml
+python3 gen_dict_with_shape.py -i ../rime-ice/cn_dicts/ext.dict.yaml     -x zrmdb -t -o ../moflice-cht/ice-dicts/flypy_zrmdb_ext.dict.yaml
+python3 gen_dict_with_shape.py -i ../rime-ice/cn_dicts/others.dict.yaml  -x zrmdb -t -o ../moflice-cht/ice-dicts/flypy_zrmdb_others.dict.yaml
+python3 gen_dict_with_shape.py -i ../rime-ice/cn_dicts/tencent.dict.yaml -x zrmdb -t -o ../moflice-cht/ice-dicts/flypy_zrmdb_tencent.dict.yaml
+cd ..
